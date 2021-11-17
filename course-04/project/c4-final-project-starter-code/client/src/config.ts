d5ffa5333bf12234697b6d92fef05bd914cdf462
Jesse Gardner <42302459+jagardner21@users.noreply.github.com>
2021-11-17 14:53:51 -0700

    Update config.ts
    updating config file for project submission

--- course-04/project/c4-final-project-starter-code/client/src/config.ts
+++ course-04/project/c4-final-project-starter-code/client/src/config.ts
@@ -1,11 +1,10 @@
 // TODO: Once your application is deployed, copy an API id here so that the frontend could interact with it
-const apiId = '...'
-export const apiEndpoint = `https://${apiId}.execute-api.us-east-1.amazonaws.com/dev`
+const apiId = '5rufxo4rlk'
+export const apiEndpoint = `https://${apiId}.execute-api.us-west-1.amazonaws.com/dev`
 
 export const authConfig = {
-  // TODO: Create an Auth0 application and copy values from it into this map. For example:
-  // domain: 'dev-nd9990-p4.us.auth0.com',
-  domain: '...',            // Auth0 domain
-  clientId: '...',          // Auth0 client id
+  // TODO: Create an Auth0 application and copy values from it into this map
+  domain: 'dev-jkk6gg15.us.auth0.com',            // Auth0 domain
+  clientId: 'UamUXQMboR1CH2RaXSmmH373rI0BxxIb',          // Auth0 client id
   callbackUrl: 'http://localhost:3000/callback'
 }
